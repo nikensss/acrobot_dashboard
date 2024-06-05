2182257785be284bea8605980d15915839e6936b
Ricard Mallafre <rikitzzz@gmail.com>
2024-06-05 13:37:17 +0200

    chore: added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
-  const [command, setCommand] = useState("");
-  const [response, setResponse] = useState("");
+  const [command, setCommand] = useState('');
+  const [response, setResponse] = useState('');
 
   return (
-    <main className="flex flex-col items-center justify-center min-w-full">
+    <main className="flex min-w-full flex-col items-center justify-center">
       <h1 className="my-12">Acrobot</h1>
       <div className="flex flex-col gap-2">
         <input
@@ -17,12 +17,10 @@ function App() {
         />
         <button
           onClick={async () => {
-            const command = document.getElementById(
-              "command",
-            ) as HTMLInputElement;
-            const response = await fetch("http://acrobot.local/test-command", {
-              method: "POST",
-              body: JSON.stringify({ command: command.value }),
+            const command = document.getElementById('command') as HTMLInputElement;
+            const response = await fetch('http://acrobot.local/test-command', {
+              method: 'POST',
+              body: JSON.stringify({ command: command.value })
             });
             const json = await response.json();
             console.log(json);
@@ -31,10 +29,7 @@ function App() {
         >
           Send test command
         </button>
-        <textarea
-          className="w-96 h-60 resize-none rounded-md"
-          value={response}
-        ></textarea>
+        <textarea className="h-60 w-96 resize-none rounded-md" value={response}></textarea>
       </div>
     </main>
   );
